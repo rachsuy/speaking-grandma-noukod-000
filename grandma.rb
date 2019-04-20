9851dc9f647f80448f2b8ecd6fb264b95600d96e
Rachele Suy <rachelesuy@gmail.com>
2019-04-20 09:27:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase =="I LOVE YOU GRANDMA!" 
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.capitalize
+  elsif phrase == phrase.upcase
   		return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.capitalize
       return "HUH?! SPEAK UP, SONNY!"
