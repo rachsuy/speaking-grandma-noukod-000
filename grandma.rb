fc58cd485ab9f23cd95d53bc9b33e424e80c2c0d
Rachele Suy <rachelesuy@gmail.com>
2019-04-20 09:35:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
   
    return "NO, NOT SINCE 1938!"
    
-  elsif phrase == phrase.downcase
+  elsif phrase == phrase.capitalize
   		return "HUH?! SPEAK UP, SONNY!"
   
   elsif phrase == phrase.capitalize
