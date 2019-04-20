491839abec62011c7e1e484df83ce29a17fe00e3
Rachele Suy <rachelesuy@gmail.com>
2019-04-20 09:25:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase =="I LOVE YOU GRANDMA!" 
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?".downcase
-   return "HUH?! SPEAK UP, SONNY! "
-    elsif phrase == "Hi".downcase
-   return "HUH?! SPEAK UP, SONNY!"
-   
+  elsif phrase == phrase.downcase
+  		return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.downcase
+      return "HUH?! SPEAK UP, SONNY!"
   elsif  phrase ==phrase.upcase
    return "NO, NOT SINCE 1938!"
    
